0d752eb20365d0f995779df9d5990b4af71c9717
Romario Negreiros <nromario482@gmail.com>
2023-08-14 17:23:56 -0300

    Add all project routes related to users handling, implement get-one route

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,73 @@
+import userController from "../controllers/user.controller";
+
+import type { FastifyBaseLogger, FastifyInstance, FastifyTypeProviderDefault, RawServerDefault } from "fastify";
+import type { IncomingMessage, ServerResponse } from "http";
+
+type TFastifyInstance = FastifyInstance<
+  RawServerDefault,
+  IncomingMessage,
+  ServerResponse<IncomingMessage>,
+  FastifyBaseLogger,
+  FastifyTypeProviderDefault
+>;
+
+function usersRoutes(app: TFastifyInstance) {
+  const baseUrl = "/api/users";
+
+  // GET ROUTES
+  app.get(
+    `${baseUrl}/get-one/:email`,
+    {
+      schema: {
+        params: {
+          email: {
+            type: "string",
+          },
+        },
+        response: {
+          200: {
+            type: "object",
+            properties: {
+              user: {
+                id: { type: "number" },
+                name: { type: "string" },
+                email: { type: "string" },
+                teams: { type: "array" },
+                created_at: { type: "string" },
+                is_active: { type: "number" }
+              }
+            },
+          },
+          404: {
+            type: "string",
+          },
+          500: {
+            type: "string"
+          }
+        },
+      },
+    },
+    userController.getOne,
+  );
+
+  // POST ROUTES
+  app.post(`${baseUrl}/register`, async (req, res) => {
+    await res.send({ bull: "shit" });
+  });
+
+  app.post(`${baseUrl}/verify-email`, async (req, res) => {
+    await res.send({ bull: "shit" });
+  });
+
+  // PUT ROUTES
+  app.put(`${baseUrl}/update/:email`, async (req, res) => {
+    await res.send({ bull: "shit" });
+  });
+
+  // DELETE ROUTES
+  app.delete(`${baseUrl}/delete/:email`, async (req, res) => {
+    await res.send({ bull: "shit" });
+  });
+}
+
+export default usersRoutes;
