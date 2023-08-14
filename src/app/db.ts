6ea23b23d7c912dad9d54c7ad9cd1beff7832945
Romario Negreiros <nromario482@gmail.com>
2023-08-14 17:17:32 -0300

    Name exported object

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,9 +1,11 @@
 import mysql from "mysql2";
 import dbConfig from "../config/db.config";
 
-export default mysql.createConnection({
+const connection = mysql.createConnection({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB,
 });
+
+export default connection;
