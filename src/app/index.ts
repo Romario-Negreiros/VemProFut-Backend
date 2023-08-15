26fc52f30c0bb6d4ff516666f099e3e444fd9e42
Romario Negreiros <nromario482@gmail.com>
2023-08-15 12:19:38 -0300

    Refactor: Add setDatabase method and db property to use mysql2 promise syntax

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,19 +1,35 @@
 import Fastify from "fastify";
 import userRoutes from "../routes/user.routes";
+import createDatabaseConnection from "./db";
+
+import type { Connection } from "mysql2/promise";
 
 interface IApp {
   fastify: ReturnType<typeof Fastify>;
+  db: Connection;
 }
 
 class App implements IApp {
   fastify: ReturnType<typeof Fastify>;
+  db!: Connection;
 
   constructor() {
     this.fastify = Fastify();
 
     this.setRoutes();
+    void this.setDatabase();
     this.runServer();
   }
+
+  private readonly setDatabase = async () => {
+    try {
+      this.db = await createDatabaseConnection();
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(`Erro ao tentar se conectar com banco de dados:\n${err.message}`);
+      }
+    }
+  };
 
   private readonly setRoutes = () => {
     userRoutes(this.fastify);
@@ -28,4 +44,4 @@ class App implements IApp {
   };
 }
 
-export default new App().fastify;
+export default new App();
