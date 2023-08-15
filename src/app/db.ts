ddb4dd1272297cd6ffabcf95fc69c9f000f040a0
Romario Negreiros <nromario482@gmail.com>
2023-08-15 12:19:56 -0300

    Refactor: Use mysql2 promises instead

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,11 +1,13 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import dbConfig from "../config/db.config";
 
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB,
-});
+const createDatabaseConnection = async () => {
+  return await mysql.createConnection({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB,
+  });
+};
 
-export default connection;
+export default createDatabaseConnection;
