e2cf3d21cea5a4048a0508efdc456f16aa56f722
Romario Negreiros <nromario482@gmail.com>
2023-08-15 12:20:53 -0300

    Refactor getOne method to use mysql2 promise syntax

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -7,10 +7,17 @@ import type { FastifyRequest, FastifyReply } from "fastify";
 
 interface IUserController {
   getOne: (req: FastifyRequest, res: FastifyReply) => Promise<void>;
+  register: (req: FastifyRequest, res: FastifyReply) => Promise<void>;
 }
 
 interface Params {
   email?: string;
+}
+
+interface RequestBodyUser {
+  name: string;
+  email: string;
+  teams: string;
 }
 
 class UserController implements IUserController {
@@ -22,14 +29,38 @@ class UserController implements IUserController {
         const user = await userServices.getOne(email);
 
         if (user === undefined) {
-          await res.status(404).send("Usuário não encontrado.");
+          return await res.status(404).send("Usuário não encontrado.");
         }
-        
-        await res.status(200).send({ user: { ...user, teams: user.teams?.split(',') } });
+
+        await res.status(200).send({ user: { ...user, teams: user.teams?.split(",") } });
       } catch (err) {
         console.log(err);
         await res.status(500).send("Erro no processamento interno ao tentar buscar o usuário.");
       }
+    }
+  };
+
+  register = async (req: FastifyRequest, res: FastifyReply): Promise<void> => {
+    const body = req.body as RequestBodyUser;
+    if (body.name === undefined || body.name === null) {
+       return await res.status(400).send("O campo 'nome' está faltando na requisição.");
+    }
+
+    if (body.email === undefined || body.email === null) {
+      return await res.status(400).send("O campo 'email' está faltando na requisição.");
+    }
+
+    if (body.teams === undefined || body.teams === null || body.teams.length === 0) {
+      return await res.status(400).send("O campo 'time(s)' está faltando na requisição.");
+    }
+
+    try {
+      await userServices.register(body);
+
+      await res.status(201).send(`Você foi registrado com sucesso, ${body.name}`);
+    } catch (err) {
+      console.log(err);
+      await res.status(500).send("Erro no processamento interno ao tentar buscar o usuário.");
     }
   };
 }
