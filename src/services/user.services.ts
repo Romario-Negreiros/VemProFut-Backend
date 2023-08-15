9eb6e0959e93da16c4536866704b69e891394516
Romario Negreiros <nromario482@gmail.com>
2023-08-14 21:08:12 -0300

    Change query to get all user's teams

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -8,8 +8,18 @@ interface IUserServices {
 
 class UserServices implements IUserServices {
   getOne = async (email: string): Promise<User> => {
+    const query = `
+      SELECT users.*, teams.teams
+      FROM users
+      LEFT JOIN (
+      SELECT user_id, GROUP_CONCAT(team) AS teams
+      FROM teams
+      GROUP BY user_id
+      ) teams ON users.id = teams.user_id
+      WHERE users.email = ?;
+    `;
     return await new Promise((resolve, reject) => {
-      connection.query<User[]>("SELECT * FROM users WHERE email=?", [email], (err, results) => {
+      connection.query<User[]>(query, [email], (err, results) => {
         if (err !== null) {
           reject(err);
         } else {
