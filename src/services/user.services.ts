1f317fdeffbd8dd7a1f755f3133be6904db63fad
Romario Negreiros <nromario482@gmail.com>
2023-08-15 18:08:01 -0300

    Refactor getOne and register methods: rewrite clauses to fit many-to-manny relationship between users and teams table, declare updateTeams method (not implemented)

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -16,15 +16,11 @@ interface IUserServices {
 class UserServices implements IUserServices {
   getOne = async (email: string): Promise<User> => {
     const query = `
-      SELECT users.*, teams.teams
-      FROM users
-      LEFT JOIN (
-      SELECT user_id, GROUP_CONCAT(team) AS teams
-      FROM teams
-      GROUP BY user_id
-      ) teams ON users.id = teams.user_id
-      WHERE users.email = ?;
-    `;
+    SELECT users.*, GROUP_CONCAT(teams.team_name) AS teams
+    FROM users
+    LEFT JOIN user_teams ON users.id = user_teams.user_id
+    LEFT JOIN teams ON user_teams.team_id = teams.id
+    WHERE email = ?`
     const [result] = await app.db.query<User[]>(query, [email]);
     return result?.[0];
   };
@@ -38,7 +34,9 @@ class UserServices implements IUserServices {
       await app.db.query("SET @user_id = LAST_INSERT_ID()");
 
       for (const team of teams.split(",")) {
-        await app.db.query("INSERT INTO teams (team, user_id) VALUES (?, @user_id)", [team]);
+        await app.db.query("INSERT IGNORE INTO teams (team_name) VALUES (?)", [team]);
+        await app.db.query("SET @team_id = (SELECT id FROM teams WHERE team_name = ?)", [team]);
+        await app.db.query("INSERT INTO user_teams (user_id, team_id) VALUES (@user_id, @team_id)");
       }
 
       await app.db.commit();
@@ -47,6 +45,9 @@ class UserServices implements IUserServices {
       throw err;
     }
   };
+
+  updateTeams = async ({ email, teams }: Omit<RequestBodyUser, 'name'>): Promise<void> => {
+  }
 }
 
 export default new UserServices();
