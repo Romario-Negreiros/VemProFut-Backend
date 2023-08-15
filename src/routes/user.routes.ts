8713dff3c6890863cfc30738a6ea69c153e55cba
Romario Negreiros <nromario482@gmail.com>
2023-08-15 18:06:13 -0300

    Add schema and controller method to updateTeams route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -93,9 +93,42 @@ function usersRoutes(app: TFastifyInstance) {
   });
 
   // PUT ROUTES
-  app.put(`${baseUrl}/update/:email`, async (req, res) => {
+  app.put(`${baseUrl}/update/email`, async (req, res) => {
     await res.send({ bull: "shit" });
   });
+
+  app.put(`${baseUrl}/update/teams`, {
+    schema: {
+      body: {
+        email: {
+          type: "string",
+        },
+        teams: {
+          type: "string"
+        }
+      },
+      response: {
+        200: {
+          type: "string",
+        },
+        400: {
+          type: "string",
+        },
+        500: {
+          type: "string",
+        },
+      },
+      headers: {
+        type: "object",
+        properties: {
+          "Content-Type": {
+            type: "string"
+          }
+        },
+        required: ["Content-Type"],
+      },
+    },
+  }, userController.updateTeams);
 
   // DELETE ROUTES
   app.delete(`${baseUrl}/delete/:email`, async (req, res) => {
