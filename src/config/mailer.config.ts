e513ff94c8e63b69523e5b1ac069f7971d415492
Romario Negreiros <nromario482@gmail.com>
2023-08-16 16:42:35 -0300

    Add config object to nodemailer

---
+++ src/config/mailer.config.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();  
+
+export default {
+  host: "smtp.zoho.com",
+  port: 465,
+  secure: true,
+  user: process.env.ZOHOMAIL_USER,
+  pass: process.env.ZOHOMAIL_PASS,
+};
