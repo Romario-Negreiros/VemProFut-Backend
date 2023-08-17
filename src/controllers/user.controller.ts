11a0f13697079ba3414ccfb21592e2d244963331
Romario Negreiros <nromario482@gmail.com>
2023-08-16 22:13:54 -0300

    Add verifyEmail controller and add token property to Params interface

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -13,6 +13,7 @@ interface IUserController {
 
 interface Params {
   email?: string;
+  token?: string;
 }
 
 interface RequestBodyUser {
@@ -62,16 +63,67 @@ class UserController implements IUserController {
     }
 
     try {
-      const verifyEmailToken = crypto.randomBytes(10).toString('hex');
+      const verifyEmailToken = crypto.randomBytes(10).toString("hex");
       const verifyEmailTokenExpiration = new Date();
       verifyEmailTokenExpiration.setHours(verifyEmailTokenExpiration.getHours() + 1);
 
       await userServices.register(body, verifyEmailToken, verifyEmailTokenExpiration.toISOString());
 
-      await res.status(201).send(`Você foi registrado com sucesso, ${body.name}.`);
+      await res
+        .status(201)
+        .send(
+          `Você foi registrado com sucesso, ${body.name}. Verifique seu email para começar a receber as notificações semanais.`,
+        );
     } catch (err) {
       console.log(err);
       await res.status(500).send("Erro no processamento interno ao tentar registrar o usuário.");
+    }
+  };
+
+  verifyEmail = async (req: FastifyRequest, res: FastifyReply): Promise<void> => {
+    const params = req.params as Params;
+    if (params.email === undefined) {
+      return await res.status(400).send("Parâmetro 'email' está vazio.");
+    }
+
+    if (params.token === undefined) {
+      return await res.status(400).send("Parâmetro 'token' está vazio.");
+    }
+
+    try {
+      const user = await userServices.getOne(params.email);
+
+      if (user === undefined) {
+        return await res.status(404).send("Usuário não encontrado.");
+      }
+
+      if (user.verify_email_token === undefined || user.verify_email_token_expiration === undefined) {
+        return await res.status(400).send("Usuário com o email já verificado.");
+      }
+
+      const now = new Date();
+      const tokenExpiration = new Date(user.verify_email_token_expiration);
+
+      if (now > tokenExpiration) {
+        await userServices.delete(user);
+
+        return await res.status(400).send("O token de validação de email expirou, crie sua conta novamente.");
+      }
+
+      if (params.token !== user.verify_email_token) {
+        return await res.status(400).send("Token inválido.");
+      }
+
+      await userServices.verifyEmail(params.email, params.token);
+
+      user.is_active = 1;
+      delete user.verify_email_token;
+      delete user.verify_email_token_expiration;
+
+      await res.status(200).send({ user: { ...user, teams: user.teams?.split(",") } });
+    } catch (err) {
+      console.log(err);
+      await res.status(500).send("Erro no processamento interno ao tentar verificar o email do usuário.");
     }
   };
 
