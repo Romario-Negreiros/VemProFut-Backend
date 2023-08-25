95e7bea9ee89d10621893c804d05d99e69d54741
Romario Negreiros <nromario482@gmail.com>
2023-08-25 11:32:04 -0300

    Create venue model

---
+++ src/models/venue.model.ts
@@ -0,0 +1,11 @@
+import type { RowDataPacket } from "mysql2";
+
+export default interface Venue extends RowDataPacket {
+  id?: number;
+  name?: string;
+  address?: string;
+  city?: string;
+  capacity?: number;
+  surface?: string;
+  image?: string;
+};
