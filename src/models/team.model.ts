ee25628131623b82e602fc04c7c32a70f780cc7d
Romario Negreiros <nromario482@gmail.com>
2023-08-25 11:29:06 -0300

    Create team model

---
+++ src/models/team.model.ts
@@ -0,0 +1,13 @@
+import type { RowDataPacket } from "mysql2";
+
+export default interface Team extends RowDataPacket {
+  id?: number;
+  name?: string;
+  code?: string;
+  country?: string;
+  translatedCountry?: string;
+  countryFlag?: string;
+  founded?: number;
+  logo?: string;
+  venueId?: number;
+};
