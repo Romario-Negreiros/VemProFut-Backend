625c752a15ddda0428075aafdf916ff9a1432ad6
Romario Negreiros <nromario482@gmail.com>
2023-08-28 17:47:27 -0300

    Refactor user model with correct teams property types

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,18 @@
 import type { RowDataPacket } from "mysql2";
+import type Team from "./team.model";
+import type Venue from "./venue.model";
 
 export default interface User extends RowDataPacket {
   id?: number;
   name?: string;
   email?: string;
-  teams?: string;
   createdAt?: string;
   isActive?: number;
   verifyEmailToken?: string;
   verifyEmailTokenExpiration?: string;
+  teams: Array<
+  {
+    venue: Venue | null;
+  } & Omit<Team, "venueId">
+> | null;
 }
