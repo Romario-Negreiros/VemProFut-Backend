ad30eff3afc5f78eea559dce075c1197434d55a1
Romario Negreiros <nromario482@gmail.com>
2023-08-28 17:49:39 -0300

    Refactor responses sent to client: whenever it is a 'not ok' response, send an object with error prop instead of just a message

--- src/controllers/team.controller/index.ts
+++ src/controllers/team.controller/index.ts
@@ -6,20 +6,20 @@ class TeamController implements ITeamController {
   getOne: Controller = async (req, res) => {
     const { id } = req.params as IParams["getOne"];
     if (id === undefined) {
-      return await res.status(400).send("Parâmetro 'id do time' está vazio.");
+      return await res.status(400).send({ error: "Parâmetro 'id do time' está vazio." });
     }
 
     try {
       const team = await teamServices.getOne(id);
 
       if (team === undefined) {
-        return await res.status(404).send("Time não encontrado.");
+        return await res.status(404).send({ error: "Time não encontrado." });
       }
 
       await res.status(200).send({ team });
     } catch (err) {
       console.log(err);
-      await res.status(500).send("Erro no processamento interno ao tentar buscar o usuário.");
+      await res.status(500).send({ error: "Erro no processamento interno ao tentar buscar o usuário." });
     }
   };
 }
