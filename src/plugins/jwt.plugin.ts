e6dad650b3888728d4eb574093bdb66edaa2a949
Romario Negreiros <nromario482@gmail.com>
2023-09-01 14:43:12 -0300

    Create plugin to validate jwt

---
+++ src/plugins/jwt.plugin.ts
@@ -0,0 +1,28 @@
+import type { FastifyRequest, FastifyReply } from "fastify";
+
+const jwtPlugin = async (req: FastifyRequest, res: FastifyReply) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return await res.status(401).send({ error: "Token de autenticação não fornecido." })
+  }
+
+  const parts = authHeader.split(" ");
+
+  if (parts.length !== 2) {
+    return await res.status(401).send({ error: "Token de autenticação mal-formatado." })
+  }
+
+  if (/^Bearer [aA-zZ0-9.,+\-?^~,/()]*$/i.test(authHeader)) {
+    return await res.status(401).send({ error: "Token de autenticação mal-formatado." })
+  }
+
+  try {
+    await req.jwtVerify({ maxAge: 86400 });
+  } catch (error) {
+    console.log(error);
+    await res.status(401).send({ error: "Token inválido" });
+  }
+};
+
+export default jwtPlugin;
