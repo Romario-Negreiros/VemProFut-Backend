80c3645783b2d54751e00420df7c5a1c04f15eb4
Romario Negreiros <nromario482@gmail.com>
2023-09-04 10:25:30 -0300

    Declare IUserServices properties' types directly

--- src/services/user.services/types.ts
+++ src/services/user.services/types.ts
@@ -1,23 +1,16 @@
 import type User from "../../models/user.model";
 
-type TUpdateTeams = (teams: string, user: User) => Promise<void>;
-
-type TCreate = (
-  name: string,
-  email: string,
-  password: string,
-  verifyEmailToken: string,
-  verifyEmailTokenExpiration: string,
-  teams?: string,
-) => Promise<void>;
-
-type TVerifyEmail = (email: string, token: string) => Promise<void>;
-
-type TDelete = (email: string) => Promise<void>;
-
 export interface IUserServices {
-  create: TCreate;
-  verifyEmail: TVerifyEmail;
-  updateTeams: TUpdateTeams;
-  delete: TDelete;
+  get: (email: string) => Promise<User | undefined>;
+  create: (
+    name: string,
+    email: string,
+    password: string,
+    verifyEmailToken: string,
+    verifyEmailTokenExpiration: string,
+    teams?: string,
+  ) => Promise<void>;
+  verifyEmail: (email: string, token: string) => Promise<void>;
+  updateTeams: (teams: string, user: User) => Promise<void>;
+  delete: (email: string) => Promise<void>;
 }
