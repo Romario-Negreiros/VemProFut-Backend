a6266c911fe76d2806744ebe56df5f29e9b3cde9
Romario Negreiros <nromario482@gmail.com>
2023-09-04 10:22:30 -0300

    Refactor get method using venuesServices to fetch data about a team's venue

--- src/services/team.services/index.ts
+++ src/services/team.services/index.ts
@@ -1,24 +1,23 @@
 import app from "../../app";
 
+import venuesServices from "../venues.services/";
+
 import type { ITeamServices } from "./types";
 import type Team from "../../models/team.model";
-import type Venue from "../../models/venue.model";
 
 class TeamServices implements ITeamServices {
-  getOne: ITeamServices["getOne"] = async (id) => {
+  get: ITeamServices["get"] = async (id) => {
     const [result] = await app.db.query<Team[]>("SELECT * FROM Teams WHERE id = ?", [id]);
+    
     if (result?.[0] === undefined) {
       return undefined;
     } else {
-      const team = result?.[0];
-      const [venueQueryResult] = await app.db.query<Venue[]>("SELECT * FROM Venues WHERE id = ?", [team.venueId]);
-      if (venueQueryResult?.[0] === undefined) {
-        delete team.venueId;
-        return { ...team, venue: null };
-      } else {
-        const venue = venueQueryResult?.[0];
-        delete team.venueId;
+      const team = result[0];
+      if (team.venueId) {
+        const venue = venuesServices.get(team.venueId);
         return { ...team, venue };
+      } else {
+        return { ...team, venue: null };
       }
     }
   };
