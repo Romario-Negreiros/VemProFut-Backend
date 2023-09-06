6de72cfe07b08aee32f86783fe35f416354d7a25
Romario Negreiros <nromario482@gmail.com>
2023-09-06 15:42:03 -0300

    Import only FastifyInstance type, use teamController.get and schemas.get instead of getOne

--- src/routes/teams/index.ts
+++ src/routes/teams/index.ts
@@ -1,27 +1,18 @@
 import teamController from "../../controllers/team.controller";
 import schemas from "./schemas";
 
-import type { FastifyBaseLogger, FastifyInstance, FastifyTypeProviderDefault, RawServerDefault } from "fastify";
-import type { IncomingMessage, ServerResponse } from "http";
+import type { FastifyInstance } from "fastify";
 
-type TFastifyInstance = FastifyInstance<
-  RawServerDefault,
-  IncomingMessage,
-  ServerResponse<IncomingMessage>,
-  FastifyBaseLogger,
-  FastifyTypeProviderDefault
->;
-
-const teamsRoutes = (app: TFastifyInstance) => {
+const teamsRoutes = (app: FastifyInstance) => {
   const baseUrl = "/api/teams";
 
   // GET ROUTES
   app.get(
-    `${baseUrl}/get-one/:id`,
+    `${baseUrl}/get/:id`,
     {
-      schema: schemas.getOne,
+      schema: schemas.get,
     },
-    teamController.getOne,
+    teamController.get,
   );
 };
 
