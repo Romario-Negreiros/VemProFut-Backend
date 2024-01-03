beb77639cdfdc2333b474892650966528370f47b
Romario Negreiros <nromario482@gmail.com>
2024-01-03 20:48:01 -0300

    Create function to seed initial database data

--- src/app/index.ts
+++ src/app/index.ts
@@ -8,13 +8,20 @@ import fastifyJWT from "@fastify/jwt";
 import jwtPlugin from "../plugins/jwt.plugin";
 
 import env from "../config/env.config";
+import teams from "../../public/data/teams.json";
+import venues from "../../public/data/venues.json";
 
 import type { Connection } from "mysql2/promise";
+import type { RowDataPacket } from "mysql2";
 import type { FastifyInstance } from "fastify";
 
 interface IApp {
   fastify: FastifyInstance & { authenticate?: any };
   db: Connection;
+}
+
+interface CountRows extends RowDataPacket {
+  totalRowsInTeamsTable: number;
 }
 
 class App implements IApp {
@@ -35,6 +42,8 @@ class App implements IApp {
   private readonly setDatabase = async () => {
     try {
       this.db = await createDatabaseConnection();
+
+      this.seedDatabase();
     } catch (error) {
       if (error instanceof Error) {
         console.log(`Erro ao tentar se conectar com banco de dados:\n${error.message}`);
@@ -63,6 +72,57 @@ class App implements IApp {
         console.log(`Erro ao rodar servidor local:\n${error.message}`);
       }
     });
+  };
+
+  private readonly seedDatabase = async () => {
+    try {
+      await this.db.beginTransaction();
+
+      const [result] = await this.db.query<CountRows[]>("SELECT COUNT(id) AS totalRowsInTeamsTable FROM TEAMS");
+      if (result?.[0]) {
+        const { totalRowsInTeamsTable } = result[0];
+        if (totalRowsInTeamsTable > 0) return;
+      }
+      for (const venue of venues) {
+        await this.db.query(
+          `
+          INSERT IGNORE INTO VENUES (
+            id,
+            name,
+            address,
+            city,
+            capacity,
+            surface,
+            image
+          ) VALUES (?, ?, ?, ?, ?, ?, ?)
+          `,
+          [venue.id, venue.name, venue.address, venue.city, venue.capacity, venue.surface, venue.image],
+        );
+      }
+
+      for (const team of teams) {
+        await this.db.query(
+          `
+          INSERT INTO TEAMS (
+            id,
+            name,
+            code,
+            country,
+            founded,
+            logo,
+            venueId
+          ) VALUES (?, ?, ?, ?, ?, ?, ?)
+        `,
+          [team.id, team.name, team.code, team.country, team.founded, team.logo, team.venueId],
+        );
+      }
+
+      await this.db.commit();
+    } catch (err) {
+      await this.db.rollback();
+
+      if (err instanceof Error) console.log(err.message);
+    }
   };
 }
 
