bdf1fcca0f91c76005f70214a9ebab2d28d233a3
Romario-Negreiros <nromario482@gmail.com>
2024-04-30 15:31:34 -0300

    Remove unused code

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,10 +1,4 @@
 import type { RowDataPacket } from "mysql2";
-// import type TeamModel from "./team.model";
-// import type Venue from "./venue.model";
-
-// interface Team extends TeamModel {
-//   venue: Venue | null;
-// }
 
 export default interface User extends RowDataPacket {
   id: number;
